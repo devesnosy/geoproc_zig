0cbcf4c283df959d584f677b6b6884fb876412a5
Eyad Ahmed <dev.esnosy@gmail.com>
2024-11-26 10:42:57 +0200

    Parse ASCII and binary "STL" mesh files

--- src/main.zig
+++ src/main.zig
@@ -170,20 +170,29 @@ pub fn Triangle3D(comptime T: type, ops: fn (type) Ops_Type(T)) type {
     };
 }
 
+pub fn read_token(reader: std.fs.File.Reader, optional_out: ?*std.ArrayList(u8)) !void {
+    if (optional_out) |out| out.clearRetainingCapacity();
+    // Skip leading whitespace
+    while (true) {
+        const char = reader.readByte() catch return;
+        if (!std.ascii.isWhitespace(char)) {
+            if (optional_out) |out| try out.append(char);
+            break;
+        }
+    }
+    // Read token until whitespace
+    while (true) {
+        const char = reader.readByte() catch return;
+        if (std.ascii.isWhitespace(char)) return;
+        if (optional_out) |out| try out.append(char);
+    }
+}
+
+pub fn skip_token(reader: std.fs.File.Reader) !void {
+    try read_token(reader, null);
+}
+
 pub fn main() !void {
-    const a = GVec(f32, 3, Default_Ops).init(.{ 1, 2, 3 });
-    const b = GVec(f32, 3, Default_Ops).init(.{ 4, 5, 6 });
-    const c = GVec(f32, 3, Default_Ops).init(.{ 7, 8, 9 });
-    const a_cross_b = a.cross(b);
-    const a_dot_b = a.dot(b);
-    const a_scaled = a.mul_s(100);
-
-    const t = Triangle3D(f32, Default_Ops).init(.{ a, b, c });
-    const n = t.calc_normal();
-    std.debug.print("{}\n", .{n});
-
-    std.debug.print("a={}, b={}, axb={}, a.b={}, a_scaled={}\n", .{ a, b, a_cross_b, a_dot_b, a_scaled });
-
     const cwd = std.fs.cwd();
     const file = try cwd.openFile("suzanne_ascii.stl", .{ .mode = .read_only });
     defer file.close();
@@ -192,9 +201,64 @@ pub fn main() !void {
     const num_tris: u64 = try reader.readInt(u32, .little);
     const file_size = try file.getEndPos();
     const expected_binary_size = num_tris * 50 + 84;
+
+    const T3D_f32 = Triangle3D(f32, Default_Ops);
+    const allocator = std.heap.page_allocator;
+    var tris = std.ArrayList(T3D_f32).init(allocator);
+    defer tris.deinit();
+
     if (file_size == expected_binary_size) {
-        std.debug.print("Binary", .{});
+        std.debug.print("Binary\n", .{});
+        for (0..num_tris) |_| {
+            try reader.skipBytes(12, .{}); // Skip normal
+            var t = T3D_f32.init(undefined);
+            for (0..3) |i| {
+                const x: f32 = @bitCast(try reader.readInt(i32, .little));
+                const y: f32 = @bitCast(try reader.readInt(i32, .little));
+                const z: f32 = @bitCast(try reader.readInt(i32, .little));
+                t.vertices[i].components = .{ x, y, z };
+            }
+            try tris.append(t);
+            try reader.skipBytes(2, .{}); // Skip "attribute byte count"
+        }
     } else {
-        std.debug.print("ASCII", .{});
+        std.debug.print("ASCII\n", .{});
+        try file.seekTo(0);
+
+        var token = std.ArrayList(u8).init(allocator);
+        defer token.deinit();
+
+        outer: while (true) {
+            try read_token(reader, &token);
+            if (token.items.len == 0) break :outer;
+            if (std.mem.eql(u8, token.items, "facet")) {
+                try skip_token(reader); // Expecting "normal"
+                for (0..3) |_| try skip_token(reader); // Skip components of 3D normal vector
+                try skip_token(reader); // Expecting "outer"
+                try skip_token(reader); // Expecting "loop"
+                var t = T3D_f32.init(undefined);
+                for (0..3) |i| {
+                    try skip_token(reader); // Expecting "vertex"
+
+                    try read_token(reader, &token);
+                    if (token.items.len == 0) break :outer;
+                    const x = try std.fmt.parseFloat(f32, token.items);
+
+                    try read_token(reader, &token);
+                    if (token.items.len == 0) break :outer;
+                    const y = try std.fmt.parseFloat(f32, token.items);
+
+                    try read_token(reader, &token);
+                    if (token.items.len == 0) break :outer;
+                    const z = try std.fmt.parseFloat(f32, token.items);
+
+                    t.vertices[i].components = .{ x, y, z };
+                }
+                try tris.append(t);
+                try skip_token(reader); // Expecting "endloop"
+                try skip_token(reader); // Expecting "endfacet"
+            }
+        }
     }
+    std.debug.print("Num tris: {}\n", .{tris.items.len});
 }
